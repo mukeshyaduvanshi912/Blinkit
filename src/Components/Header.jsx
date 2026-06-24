cb8add1b169503a64cb6e103f10c006117d2c708
Mukesh Yadav <mukeshyaduvanshi912@gmail.com>
2026-06-24 11:00:43 +0530

    Updated App.css and responsive layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,12 +7,21 @@ function Header() {
 
       <div className="navbar-links">
         <Link to="/">Home</Link>
+
         <Link to="/about">About Us</Link>
+
         <Link to="/contact">Contact Us</Link>
+
         <Link to="/login">Login</Link>
-        <Link to="/add-product">Add Product</Link>
+
         <Link to="/register">Register</Link>
+
+        <Link to="/admin">Admin</Link>
+
+        <Link to="/add-product">Add Product</Link>
+
         <Link to="/users">Users</Link>
+
         <Link to="/cart" className="cart-link">
           Cart 🛒
         </Link>
